d83e85513e04ab4a2eb82d83022a72810492a716
Vishnu Ganesan <vishnukg@gmail.com>
2023-06-30 12:06:29 +1000

    New finds

--- app/studio_game.rb
+++ app/studio_game.rb
@@ -1,3 +1,4 @@
+require_relative 'magic_ball'
 name1 = 'larry'
 health1 = 60
 
@@ -5,4 +6,8 @@ puts "#{name1}'s health is #{health1 / 9.0}"
 
 puts "Players: \n\tlarry\n\tcurly\n\tmoe"
 
-puts "Hello, there #{larry}"
+puts "Hello, there #{name1}"
+
+halp = MagicBall.new
+
+puts halp.ask('what')
